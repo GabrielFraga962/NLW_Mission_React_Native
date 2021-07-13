881b9dc873805f25a45383b5dae3358220de3c04
Gabriel S. Fraga <gabrielsouzafraga@outlook.com>
2021-07-12 22:14:41 -0300

    Upadate Project

--- gameplayapp/src/screens/Home/index.tsx
+++ gameplayapp/src/screens/Home/index.tsx
@@ -1,12 +1,29 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import { CategorySelect } from "../../components/CategorySelect";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
 import { Profile } from "../../components/Profile";
 import { styles } from "./styles";
 
 export function Home() {
   const [category, setCategory] = useState("");
+
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20:40h",
+      description: "É hoje que vamos chagar ao challenger sem perder uma partida da md10",
+    },
+  ];
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
@@ -24,7 +41,16 @@ export function Home() {
           setCategory={handleCategorySelect}
         />
 
-        <View style={styles.content}></View>
+        <View style={styles.content}>
+          <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Appointment data={item}/>
+            )}
+          />
+        </View>
       </View>
     </View>
   );
